0e37c86b8945f404199b45b0535f977ef2cb8986
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-10-27 00:25:54 +0100

    initial commit

---
+++ contracts/AddressBook.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract AddressBook {
+    mapping(address => string) public addressToAlias;
+    mapping(string => address) public aliasToAddress;
+
+    function addAlias(string memory _alias) public {
+        // require (bytes(aliasToAddress[_alias]).length == 0, "Alias Taken");
+        require(aliasToAddress[_alias] == address(0), "Alias Taken");
+        delete aliasToAddress[addressToAlias[msg.sender]];
+        addressToAlias[msg.sender] = _alias;
+        aliasToAddress[_alias] = msg.sender;
+    }
+    function getMyAlias() public view returns (string memory) {
+        return addressToAlias[msg.sender];
+    }
+    function getAlias(address _addr1) public view returns (string memory) {
+        return addressToAlias[_addr1];
+    }
+}
