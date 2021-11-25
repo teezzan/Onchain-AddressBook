a87e11ee35a608cfd5ef7b4c612a71c76333f815
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-11-25 19:59:44 +0100

    fix deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,18 @@
 const main = async () => {
-    const addressContractFactory = await hre.ethers.getContractFactory('AddressBook');
-    const addressContract = await addressContractFactory.deploy({
-        // value: hre.ethers.utils.parseEther('1'),
-    });
-    await addressContract.deployed();
-    console.log('Contract addy:', addressContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const addressContractFactory = await hre.ethers.getContractFactory('AddressBook');
+  const addressContract = await addressContractFactory.deploy({});
+  await addressContract.deployed();
+  console.log('Contract Address:', addressContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
